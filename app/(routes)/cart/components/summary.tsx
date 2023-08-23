69754b3bbf28cada2b7c2c0ebc3a9d315fe59bfc
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-08-23 18:09:43 +0200

    disable checkout button on order summary if no items are available

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -39,7 +39,7 @@ const Summary = () => {
 
     return (
         <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-            <h2 className="text-lg font-medium text-gray-500">Order Summary</h2>
+            <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
             <div className="mt-6 space-y-4">
                 <div className="flex items-center justify-between border-t border-gray-200 pt-4">
                     <div className="text-base font-medium text-gray-900">
@@ -48,7 +48,7 @@ const Summary = () => {
                     <Currency value={totalPrice}/>
                 </div>
             </div>
-            <Button onClick={onCheckOut} className="w-full mt-6">
+            <Button onClick={onCheckOut} disabled={items.length === 0} className="w-full mt-6">
                  Checkout
             </Button>
         </div>
